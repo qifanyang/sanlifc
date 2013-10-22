ba66b0a375f552d5ad892e0e2324f9e8bbd41f97
qifanyang <584874132@qq.com>
2013-10-22 14:46:50 +0800

    add fcbean

---
+++ sanlifc/src/com/sanli/dao/FCBean.java
@@ -0,0 +1,60 @@
+package com.sanli.dao;
+
+import java.sql.Date;
+
+/**
+ * 对应数据库表的实体类
+ */
+public class FCBean {
+	public int id;
+	public String city;
+	public int year;
+	public String create_project_title;
+	public String create_project_number;
+	public String project_name;
+	public String mis_number;
+	public String research_contract_number;
+	public String simple_name;
+	public String project_main_type;
+	public String project_type;
+	public String create_project_info;
+	public String a_project_manager;
+	public String project_leader;
+	public String contract_pay_info;
+	public String project_total_invest;
+	public String ht_wireless;
+	public String ht_transmisson;
+	public String ht_power;
+	public String ht_civil;
+	public String ht_total;
+	public String a_time;
+	public String a_note;
+	public String b_time;
+	public String b_note;
+	public String c_time;
+	public String c_note;
+	public String d_time;
+	public String d_note;
+	public String e_time;
+	public String e_note;
+	public String f_time;
+	public String f_note;
+	public String check_info;
+	public Date final_cost_time;
+	public float final_wireless;
+	public float final_transmission;
+	public float final_power;
+	public float final_civil;
+	public float final_total;
+	public Date fapiao_a_time;
+	public float fapiao_a_scale;
+	public float fapiao_a_money;
+	public Date fapiao_b_time;
+	public float fapiao_b_scale;
+	public float fapiao_b_money;
+	public Date fapiao_c_time;
+	public float fapiao_c_scale;
+	public float fapiao_c_money;
+	public String remark;
+
+}
