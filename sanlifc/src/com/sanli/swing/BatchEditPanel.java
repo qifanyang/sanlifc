bccc150d2ff0e591787ecaee411767950c4b4931
qifanyang <584874132@qq.com>
2013-10-29 17:33:42 +0800

    准备把ShowPanel抽出来,需要修改下结构了,原来的结构太差

--- sanlifc/src/com/sanli/swing/BatchEditPanel.java
+++ sanlifc/src/com/sanli/swing/BatchEditPanel.java
@@ -5,6 +5,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 import java.lang.reflect.Field;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Vector;
 
@@ -31,8 +32,10 @@ public class BatchEditPanel extends JPanel {
 
 	private static BatchEditPanel instance = new BatchEditPanel();
 	public JTable table;
-	Vector<String> header = new Vector<String>();
-	JPanel batchShowPanel = new JPanel();
+	public Vector<String> header = new Vector<String>();
+	public JPanel batchShowPanel = new JPanel();
+	//用于批量导入数据-->编辑数据-->导出数据
+	public List<FCBean> beanList = new ArrayList<FCBean>();
 
 	private BatchEditPanel() {
 		setLayout(new BorderLayout());
@@ -50,7 +53,7 @@ public class BatchEditPanel extends JPanel {
 		add(inAndOut, BorderLayout.NORTH);
 		add(scrollPane, BorderLayout.CENTER);
 		
-		inBtn.addActionListener(new ImportExcelAction());
+		inBtn.addActionListener(new BatchImportExcelAction());
 	}
 
 	public static BatchEditPanel getInstance() {
@@ -61,6 +64,7 @@ public class BatchEditPanel extends JPanel {
 		if (list == null || list.size() <= 0) {
 			return;
 		}
+		this.beanList = list;//
 		Vector<Vector<String>> rr = new Vector<Vector<String>>();
 		try {
 			for (FCBean bean : list) {
@@ -71,16 +75,13 @@ public class BatchEditPanel extends JPanel {
 						addHeaderName(f.getName());
 						Class<?> type = f.getType();
 						if (type == int.class) {
-							r.add(String.valueOf(f.getInt(bean) <= 0 ? "" : f
-									.getInt(bean)));
+							r.add(String.valueOf(f.getInt(bean) <= 0 ? "" : f.getInt(bean)));
 						} else if (type == long.class) {
 							r.add(Utils.millisecondToDate(f.getLong(bean)));
 						} else if (type == float.class) {
-							r.add(String.valueOf(f.getFloat(bean) <= 0 ? "" : f
-									.getFloat(bean)));
+							r.add(String.valueOf(f.getFloat(bean) <= 0 ? "" : f.getFloat(bean)));
 						} else if (type == String.class) {
-							r.add(String.valueOf(f.get(bean) == null ? "" : f
-									.get(bean)));
+							r.add(String.valueOf(f.get(bean) == null ? "" : f.get(bean)));
 						}
 					}
 				}
@@ -114,7 +115,7 @@ public class BatchEditPanel extends JPanel {
 		}
 	}
 	
-	class ImportExcelAction implements ActionListener{
+	class BatchImportExcelAction implements ActionListener{
 
 		public void actionPerformed(ActionEvent e) {
 			
@@ -166,4 +167,59 @@ public class BatchEditPanel extends JPanel {
 			}
 		}
 	}
+	
+	class BatchExportExcelAction implements ActionListener{
+
+		public void actionPerformed(ActionEvent e) {
+			
+			
+			JFileChooser fileChooser = new JFileChooser();
+			fileChooser.setDialogType(JFileChooser.OPEN_DIALOG);
+			fileChooser.setDialogTitle("导出数据");
+			// fileChooser.setApproveButtonText("保存");
+			// editor.getStyledDocument().getDefaultRootElement();
+			fileChooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+			fileChooser.setFileFilter(new FileFilter() {
+				@Override
+				public boolean accept(File f) {
+					return f.getName().toLowerCase().endsWith(".xls") || f.getName().toLowerCase().endsWith(".xm") || f.isDirectory();
+				}
+
+				@Override
+				public String getDescription() {
+					return "输入到处的Excel文件名";
+				}
+			});
+			int returnVal = fileChooser.showSaveDialog(getParent());
+			if(returnVal == JFileChooser.APPROVE_OPTION) {
+				File selectedFile = fileChooser.getSelectedFile();
+				String filePath = selectedFile.getPath();
+				if(!filePath.toLowerCase().endsWith(".xls")) {
+					// 处理文件名为
+					filePath = filePath + ".xls";
+				}
+
+				log.info("导出数据 , filePath = " + filePath);
+				
+				try {
+//					 success = false;
+					 List<FCBean> list = AssetManager.getInstance().importExcel(filePath);
+					if(list.size() <= 0){
+						AppWinUtils.showWarnMsg("Excel文件中可能没有数据 !");
+					}else{
+//						AppWinUtils.showNormalMsg("导出数据成功,可直接复制到Excel中查看,文件路径[" + filePath +"]");
+						AppWinUtils.showNormalMsg("导入Excel数据成功,文件路径[" + filePath +"]");
+						BatchEditPanel.getInstance().refreshTable(list);
+					}
+				} catch(Exception e1) {
+					log.error("导出数据错误 , " + e1.getMessage());
+				}finally{
+//					ExportDialog.getInstance().setVisible(false);
+				}
+
+			}
+		}
+	}
+	
+	
 }
